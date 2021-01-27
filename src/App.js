26b6c2a979687937cd589be92bcb11a84f06d809
sale1234 <jevremovicsasa@gmail.com>
2021-01-27 15:13:24 +0100

    tasks and fa-fa icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Odlazak u teretanu',
+      day: 'Petak u 14:00pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Prosetati psa',
+      day: 'Ponedeljak u 10:00am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Ciscenje sobe',
+      day: 'Subota u 09:00am',
+      reminder: false,
+    },
+  ])
   return (
     <div className='container'>
       <Header title='Task Tracker' />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   )
 }
